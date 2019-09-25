74f99b6c4540b448ab8e7fe9117bf55ed0b781b7
David Michael <david.michael@meredith.com>
2019-09-25 12:23:16 -0500

    Check timestamp to see if last login was more than 1 year ago

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,14 @@
-import stashy
 import os
+import stashy
+from datetime import datetime, timedelta
 
 stash = stashy.connect(os.environ['BB_HOST'], os.environ['BB_USER'], os.environ['BB_PASSWORD'])
 for user in stash.admin.users.list():
-    print(user)
+    if user['directoryName'] == 'Delegated LDAP Authentication':
+        try:
+            last_login = datetime.fromtimestamp(user['lastAuthenticationTimestamp']/1000)
+            time_diff = datetime.now() - last_login
+            if time_diff > timedelta(days=365):
+                print('%s - %s' % (user['displayName'], last_login.ctime()))
+        except:
+                print(user['displayName'])
