089724baf9e970649d6bbdc7679392b7ff2efbb5
David Michael <david.michael@meredith.com>
2020-04-30 09:10:24 -0500

    Add actual user deletion

--- cleanup.py
+++ cleanup.py
@@ -1,14 +1,23 @@
 import os
+import click
 import stashy
 from datetime import datetime, timedelta
 
 stash = stashy.connect(os.environ['BB_HOST'], os.environ['BB_USER'], os.environ['BB_PASSWORD'])
+
+users_for_cleanup = []
+
 for user in stash.admin.users.list():
     if user['directoryName'] == 'Delegated LDAP Authentication':
         try:
             last_login = datetime.fromtimestamp(user['lastAuthenticationTimestamp']/1000)
             time_diff = datetime.now() - last_login
             if time_diff > timedelta(days=365):
-                print('%s - %s' % (user['displayName'], last_login.ctime()))
+                users_for_cleanup.append(user)
         except:
-                print(user['displayName'])
+                users_for_cleanup.append(user)
+
+
+for old_user in users_for_cleanup:
+    if click.confirm(f"Delete {old_user['name']}?",default=True):
+        stash.admin.users.delete(old_user['name'])
